c2bbe8f67dc4f772c173a21bf83d9de1471268c1
kachraz <kachraz@tuta.io>
2024-08-27 12:01:10 +0530

    FixImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import SolarSystem from "./sso/SSO";
-import headerImage from "./assets/kl.gif";
-import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
 // -------------------
 
@@ -52,7 +50,7 @@ function Header() {
 function CoreConcept(props) {
   return (
     <li>
-      <img src={props.img} alt={props.title} />
+      <img src={props.image} alt={props.title} />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
@@ -67,26 +65,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              img={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              img={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              img={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              img={CORE_CONCEPTS[3].image}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
