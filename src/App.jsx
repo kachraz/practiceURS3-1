7458f337765537e12c01307079f14d9db83b2db4
kachraz <kachraz@tuta.io>
2024-08-27 08:29:54 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import SolarSystem from "./SSO";
+import SolarSystem from "./sso/SSO";
+// -------------------
+
+// Header Function
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>Panty Essentials</h1>
+      <p>
+        Fundamental panty concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>Panty Essentials</h1>
-        <p>
-          Fundamental panty concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
